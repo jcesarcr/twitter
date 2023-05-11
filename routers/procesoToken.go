320dcb6eeb68f0f2d13c5d0ac8bf236121ddd483
Covarrubias Rodriguez <jcovarrubiasrodri@deloitte.com>
2023-05-11 16:27:56 -0600

    mas avances de codigo

---
+++ routers/procesoToken.go
@@ -0,0 +1,44 @@
+package routers
+
+import (
+	"errors"
+	"strings"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/jcesarcr/twitter/bd"
+	"github.com/jcesarcr/twitter/models"
+)
+
+/*Email valor de email usado en todos los endpoints*/
+var Email string
+
+/*IDUsuario es el ID devuelto del modelo que se usara en todos los endpoints*/
+var IDUsuario string
+
+/*ProcesoToken proceso token para extraer sus valores*/
+func ProcesoToken(tk string) (*models.Claim, bool, string, error) {
+	miClave := []byte("MastersdelDesarrollo_grupodeFacebook")
+	claims := &models.Claim{}
+
+	splitToken := strings.Split(tk, "Bearer")
+	if len(splitToken) != 2 {
+		return claims, false, string(""), errors.New("formato de token invalido")
+	}
+	tk = strings.TrimSpace(splitToken[1])
+	tkn, err := jwt.ParseWithClaims(tk, claims, func(token *jwt.Token) (interface{}, error) {
+		return miClave, nil
+	})
+	if err == nil {
+		_, encontrado, _ := bd.ChequeoYaExisteUsuario(claims.Email)
+		if encontrado == true {
+			Email = claims.Email
+			IDUsuario = claims.ID.Hex()
+		}
+		return claims, encontrado, IDUsuario, nil
+	}
+	if !tkn.Valid {
+		return claims, false, string(""), errors.New("token invalido")
+	}
+	return claims, false, string(""), err
+
+}
